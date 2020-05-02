f525bd1717bffbd69ac626a91dab6560c2434dd3
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-05-02 12:14:55 +0100

    event listeners made passive

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ new fullpage("#fullpage", {
       // select all the spans and add event listeners for touch and mouse events
 			const spans = document.querySelectorAll("span");
 			spans.forEach((span) => {
-				span.addEventListener("mouseover", shiftColour);
-				span.addEventListener("touchenter", shiftColour);
-				span.addEventListener("mouseout", resetColour);
-				span.addEventListener("touchleave", resetColour);
-				span.addEventListener("touchcancel", resetColour);
+				span.addEventListener("mouseover", shiftColour, {passive: true});
+				span.addEventListener("touchenter", shiftColour, {passive: true});
+				span.addEventListener("mouseout", resetColour, {passive: true});
+				span.addEventListener("touchleave", resetColour, {passive: true});
+				span.addEventListener("touchcancel", resetColour, {passive: true});
 			});
 		}
 	  // function for contact form ajax functionality
@@ -74,7 +74,7 @@ new fullpage("#fullpage", {
       // adds in alert success bs styling and makes x visible
       formAlert.classList.add("alert-success");
 			formAlert.lastElementChild.hidden = false;
-			formAlert.lastElementChild.addEventListener("click", closeAlert);
+			formAlert.lastElementChild.addEventListener("click", closeAlert, {passive: true});
 		}
 
 		function error() {
@@ -82,7 +82,7 @@ new fullpage("#fullpage", {
       // adds in alert success bs styling and makes x visible
 			formAlert.classList.add("alert-danger");
 			formAlert.lastElementChild.hidden = false;
-			formAlert.lastElementChild.addEventListener("click", closeAlert);
+			formAlert.lastElementChild.addEventListener("click", closeAlert, {passive: true});
 		}
 
     // handle the closing of the alert box
@@ -119,78 +119,3 @@ function ajax(method, url, data, success, error) {
 }
 
 fullpage_api.setAllowScrolling(true);
-
-// FULLPAGE.JS OPTIONS FOR REFERENCE
-// //Navigation
-// menu: '#menu',
-// lockAnchors: false,
-// anchors:['firstPage', 'secondPage'],
-// navigation: false,
-// navigationPosition: 'right',
-// navigationTooltips: ['firstSlide', 'secondSlide'],
-// showActiveTooltip: false,
-// slidesNavigation: false,
-// slidesNavPosition: 'bottom',
-
-// //Scrolling
-// css3: true,
-// scrollingSpeed: 700,
-// autoScrolling: true,
-// fitToSection: true,
-// fitToSectionDelay: 1000,
-// scrollBar: false,
-// easing: 'easeInOutCubic',
-// easingcss3: 'ease',
-// loopBottom: false,
-// loopTop: false,
-// loopHorizontal: true,
-// continuousVertical: false,
-// continuousHorizontal: false,
-// scrollHorizontally: false,
-// interlockedSlides: false,
-// dragAndMove: false,
-// offsetSections: false,
-// resetSliders: false,
-// fadingEffect: false,
-// normalScrollElements: '#element1, .element2',
-// scrollOverflow: false,
-// scrollOverflowReset: false,
-// scrollOverflowOptions: null,
-// touchSensitivity: 15,
-// bigSectionsDestination: null,
-
-// //Accessibility
-// keyboardScrolling: true,
-// animateAnchor: true,
-// recordHistory: true,
-
-// //Design
-// controlArrows: true,
-// verticalCentered: true,
-// sectionsColor : ['#ccc', '#fff'],
-// paddingTop: '3em',
-// paddingBottom: '10px',
-// fixedElements: '#header, .footer',
-// responsiveWidth: 0,
-// responsiveHeight: 0,
-// responsiveSlides: false,
-// parallax: false,
-// parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-// cards: false,
-// cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
-
-// //Custom selectors
-// sectionSelector: '.section',
-// slideSelector: '.slide',
-
-// lazyLoading: true,
-
-// //events
-// onLeave: function(origin, destination, direction){},
-// afterLoad: function(origin, destination, direction){},
-// afterRender: function(){},
-// afterResize: function(width, height){},
-// afterReBuild: function(){},
-// afterResponsive: function(isResponsive){},
-// afterSlideLoad: function(section, origin, destination, direction){},
-// onSlideLeave: function(section, origin, destination, direction){}
